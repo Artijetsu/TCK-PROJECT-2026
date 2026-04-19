2ab921381479de43d7ad9b74396e2e40894ad994
Artijetsu <buhankacopyright@gmail.com>
2026-04-19 20:23:39 +0300

    Delete проект-итоговый directory

--- проект-итоговый/SCENE/button.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-func _on_quit_btn_pressed():
-	print("Кнопка Выход нажата.")
-	# Завершаем работу приложения
-	get_tree().quit()
--- проект-итоговый/SCENE/quit_btn.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-func _on_pressed() -> void:
-	print("Кнопка Выход нажата.")
-	# Завершаем работу приложения
-	get_tree().quit()
